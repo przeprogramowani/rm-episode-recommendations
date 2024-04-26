b5d50d4e67189848145a07fb631fdadbeee352ed
psmyrdek <przemek.smyrdek@gmail.com>
2024-04-26 10:56:17 +0200

    feat: register federated module

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
 
 export default defineConfig({
   build: {
     sourcemap: true,
+    target: 'esnext',
   },
   server: {
     port: 3000,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: 'remote-app',
+      filename: 'recommendations.js',
+      exposes: {
+        './EpisodeRecommendations': './src/components/EpisodeRecommendations.tsx',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+  ],
 });
