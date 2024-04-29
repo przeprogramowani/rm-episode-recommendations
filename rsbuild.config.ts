1731b7b85e495e83b0304c48312dbb1a4868caae
psmyrdek <przemek.smyrdek@gmail.com>
2024-04-29 12:28:29 +0200

    feat: client config

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,29 +1,23 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 import { clientModuleFederationPlugin } from './module-federation/client-config';
-import { serverModuleFederationPlugin } from './module-federation/server-config';
 
-const isClientBuild = process.env.MF_MODE_CLIENT === 'true';
-
-export default defineConfig(async ({ env }) => ({
+export default defineConfig({
   source: {
     entry: {
       app: './src/main.tsx',
     },
   },
   output: {
-    cleanDistPath: env === 'production',
-    assetPrefix: 'https://d2mv4g2jxahmy1.cloudfront.net/mf/',
+    assetPrefix: process.env.MF_BASE_URL || 'https://localhost:3005',
     distPath: {
-      root: isClientBuild ? 'dist/mf/client' : 'dist/mf/server',
+      root: 'dist/mf/client',
     },
   },
-  targets: [isClientBuild ? 'web' : 'node'],
   tools: {
-    rspack: (config, { appendPlugins }) => {
-      config.target = isClientBuild ? 'web' : 'node';
-      appendPlugins(isClientBuild ? clientModuleFederationPlugin : serverModuleFederationPlugin);
+    rspack: (_, { appendPlugins }) => {
+      appendPlugins(clientModuleFederationPlugin);
     },
   },
   plugins: [pluginReact()],
-}));
+});
